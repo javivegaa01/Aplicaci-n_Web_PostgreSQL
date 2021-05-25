00374f8e34c69e8bc19d7e27958be986a3df8f6e
Javier Vega Correa <javier@debian>
2021-05-25 19:51:56 +0200

    terminada

--- app.py
+++ app.py
@@ -18,13 +18,24 @@ def postgres():
     p=request.form.get("p")
     conexion=psycopg2.connect(host="192.168.1.42",database=d,user=u,password="usuario")
     cur = conexion.cursor()
-    #cur.execute( "SELECT nombre, apellidos FROM medicos" )
-    cur.execute( "\l" )
-    for a in cur.fetchall() :
-        mostrar.append(a)
-    conexion.close()
-    return render_template("postgres.html",mostrar=mostrar,d=d)
+    lista_tablas=['medicos','pacientes','horasdeconsulta','salasdeconsultas']
+    lista_columnas=[]
+    for a in lista_tablas:
+        cur.execute("select column_name from information_schema.columns where table_name='%s';" % a)
+        lista=[]
+        for a in cur.fetchall():
+            lista.append(str(a))
+        lista_columnas.append(lista)
+    lista_resultados=[]
+    for a in lista_tablas:
+        cur.execute("select * from %s;" % a)
+        lista=[]
+        for a in cur.fetchall():
+            lista.append(a)
+        lista_resultados.append(lista)
+    return render_template("postgres.html",d=d,lista_tablas=lista_tablas,lista_columnas=lista_columnas,num=len(lista_tablas),lista_resultados=lista_resultados)
 
-#port=os.environ["PORT"]
-#'0.0.0.0',int(port)
-app.run(debug=True)
+port=os.environ["PORT"]
+app.run('0.0.0.0',int(port),debug=False)
+
+
